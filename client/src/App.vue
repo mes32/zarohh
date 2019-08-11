d418cb903a2bb0f7dbebe87c847eb0212a598571
mes32 <stockman.mike@gmail.com>
2019-08-11 16:32:09 -0500

    Setup client-side router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    <h1>Zarohh</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/login">Login</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-
+export default {
+  name: 'app'
+}
 </script>
 
 <style>
@@ -16,5 +22,20 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  font-size: 1.4rem;
+  color: #2c3e50;
+  margin-right: 10px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
