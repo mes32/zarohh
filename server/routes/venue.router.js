bb108db7823e0a452de62459a3c997528e5fc785
mes32 <stockman.mike@gmail.com>
2019-08-12 09:24:46 -0500

    Add connection to MongoDB in venue.router.js

--- server/routes/venue.router.js
+++ server/routes/venue.router.js
@@ -20,6 +20,38 @@ const venues = [
     }
 ];
 
+var mongoose = require("mongoose");
+var databaseURI = process.env.MONGOLAB_URI || 'mongodb://localhost/zarohh';
+
+mongoose.connect(databaseURI, function (err, res) {
+    if (err) {
+        console.log('ERROR connecting to: ' + databaseURI + '. ' + err);
+    } else {
+        console.log('Succeeded connected to: ' + databaseURI);
+    }
+});
+
+const venueSchema = new mongoose.Schema({
+    name: String
+});
+
+const venueModel = mongoose.model('venues', venueSchema);
+const newVenue = new venueModel({
+    name: 'New Venue !!'
+});
+
+newVenue.save(function (err) { if (err) console.log('Error on save!') });
+
+venueModel.find({}).exec((err, result) => {
+    if (err) {
+        console.log('Found error in venue find');
+        console.log(err.message);
+    } else {
+        console.log('Result from venue find');
+        console.log(result);
+    };
+});
+
 router.get('/', (req, res) => {
     res.send(venues);
 });
